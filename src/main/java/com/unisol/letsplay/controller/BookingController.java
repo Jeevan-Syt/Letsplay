0b01bd88f84d3aae5c6faa156351f2588214d742
Jeevan <jeevanm7971@gmail.com>
2024-10-18 15:25:38 +0530

    Changed allignments

--- src/main/java/com/unisol/letsplay/controller/BookingController.java
+++ src/main/java/com/unisol/letsplay/controller/BookingController.java
@@ -23,15 +23,21 @@ public class BookingController {
         bookingMapper.saveBooking(booking); // Insert the booking into the database
         return new ResponseEntity<>("Court booked successfully.", HttpStatus.CREATED);
     }
-    //API to get list of bookings of a user
-        @GetMapping
-        public List<Booking> getBookingsByUserId(@RequestParam("user") int userId) {
-            return bookingMapper.findBookingsByUserId(userId);
-        }
-    //API to get all booking details with time and price for booking ID
-        @GetMapping("/details")
-        public List<BookingDetailsDTO> getfindAllBookingDetailsById(@RequestParam("bookingId") int bookingId){
-            return bookingMapper.findAllBookingDetailsById(bookingId);
 
-        }
+    /**
+     * API to get list of bookings of a user
+     **/
+    @GetMapping
+    public List<Booking> getBookingsByUserId(@RequestParam("user") int userId) {
+        return bookingMapper.findBookingsByUserId(userId);
+    }
+
+    /**
+     * API to get all booking details with time and price for booking ID
+     **/
+    @GetMapping("/details")
+    public List<BookingDetailsDTO> getfindAllBookingDetailsById(@RequestParam("bookingId") int bookingId) {
+        return bookingMapper.findAllBookingDetailsById(bookingId);
+
+    }
 }
